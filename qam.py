88c6183a391ac9a33ba20e0e52cc70482a447792
waleriant <tranthien.valery@gmail.com>
2025-08-21 17:44:19 +0200

    added the xlsx functionality

--- qam.py
+++ qam.py
@@ -3,6 +3,8 @@ from quiz import Quiz, Question
 from sys import argv
 from get_user import get_user
 from random import shuffle
+from openpyxl import load_workbook
+
 
 def add_qa(user):
     try:                                                  
@@ -13,12 +15,28 @@ def add_qa(user):
         exit()
     
     tags = argv[4:]
-
     qa = QA(q, a, tags)
     user.add_qa(qa)
 
 def add_qa_from_xlsx(user):
-    pass
+    try:
+        filename = argv[2]
+    except:
+        print('Argument missing (.xlsx filename).')
+        exit()
+
+    tags = argv[3:]
+
+    wb = load_workbook(filename = filename)
+    ws = wb.active
+    length = len(list(ws.rows))
+    lines = [(ws.cell(i, 1).value, ws.cell(i, 2).value) for i in range(1,20)]
+    
+    for line in lines:
+        q = line[0]
+        a = line[1]
+        qa = QA(q, a, tags)
+        user.add_qa(qa)
 
 def display_qa_list(user):
     tags = argv[2:]
@@ -29,6 +47,10 @@ def generate_quiz(user):
     tags = argv[2:]
 
     qa_list = user.generate_qa_list(tags = tags)
+    if qa_list == []:
+        print('No Q&A available.')
+        exit()
+
     shuffle(qa_list)
     question_list = [Question(qa[1], qa[2], qa[3]) for qa in qa_list]
 
@@ -50,24 +72,25 @@ if __name__ == '__main__':
     user = get_user()
     
     try:
-        match argv[1]:
-            case '-a':
-                add_qa(user)
-            case '-ax':
-                add_qa_from_xlsx(user)            
-            case '-d':
-                display_qa_list(user)
-            case '-q':
-                generate_quiz(user)
-            case '-t':
-                display_tags(user)
-            case '-rm':
-                remove(user)
-            case '-su':
-                switch_user()
-            case _:
-                print('The first parameter is not appropriate.')
-
+        arg1 = argv[1]
     except:
         print('Missing parameters.')
         exit()
+
+    match arg1:
+        case '-a':
+            add_qa(user)
+        case '-ax':
+            add_qa_from_xlsx(user)            
+        case '-d':
+            display_qa_list(user)
+        case '-q':
+            generate_quiz(user)
+        case '-t':
+            display_tags(user)
+        case '-rm':
+            remove(user)
+        case '-su':
+            switch_user()
+        case _:
+            print('The first parameter is not appropriate.')
